1edac67773acaf186ee4fece94d44738959c43d0
ubunknown1 <ubunknown1@protonmail.com>
2022-12-30 01:43:32 +0100

    initial commit

---
+++ script/install-packages.sh
@@ -0,0 +1,4 @@
+virt-what libglvnd-dev pkg-config nautilus-admin plymouth-themes software-properties-common build-essential gnupg gnupg1 gnupg2 kgpg git gh ubuntu-restricted-extras openssh-client openssh-server ufw guake tor python3.10-full snap snapd timeshift gnome-tweaks gcc g++ openjdk-18-jre-headless gedit macchanger iw bleachbit python3.10-full libglvnd-dev pkg-config python3-pip
+
+
+
